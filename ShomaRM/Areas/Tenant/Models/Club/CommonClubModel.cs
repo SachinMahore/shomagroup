20bcfda15dae950c4594a306ca0cff5509660572
lalitbokde <lalitthinker@gmail.com>
2020-02-03 18:49:51 +0530

    Changes Done

---
+++ ShomaRM/Areas/Tenant/Models/Club/CommonClubModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShomaRM.Areas.Tenant.Models.Club
+{
+    public class CommonClubModel
+    {
+        public CommonClubModel()
+        {
+            _ClubModel = new ClubModel();
+            _clublist = new List<ClubModel>();
+        }
+       public ClubModel _ClubModel { get;set; }
+       public List<ClubModel> _clublist { get; set; }
+    }
+}
