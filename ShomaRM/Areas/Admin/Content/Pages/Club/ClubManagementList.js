1aff0a6c031b96e438be0b2e5d23409b13eb3fbe
lalitbokde <lalitthinker@gmail.com>
2020-02-11 13:51:18 +0530

    Club Admin Changes

---
+++ ShomaRM/Areas/Admin/Content/Pages/Club/ClubManagementList.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+    getGuestRegistrationList();
+});
+function ToJavaScriptDate(value) {
+    var pattern = /Date\(([^)]+)\)/;
+    var results = pattern.exec(value);
+    var dt = new Date(parseFloat(results[1]));
+    return (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear();
+}
+var getGuestRegistrationList = function () {
+    $("#divLoader").show();
+    var model = {
+        FromDate: $('#txtFromDate').val(),
+        ToDate: $('#txtToDate').val()
+
+    }
+    $.ajax({
+        url: '/ClubManagement/GetClubManagementList',
+        method: "post",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify(model),
+        success: function (response) {
+            $("#tblClubManagement>tbody").empty();
+            debugger
+            $.each(response.model, function (index, elementValue) {
+                var DateTime = ToJavaScriptDate(elementValue.StartDate); 
+                var html = '';
+                html += '<tr data-value="' + elementValue.Id + '">';
+                html += '<td style="color:#3d3939;">' + elementValue.ClubTitle + '</td>';
+                html += '<td style="color:#3d3939;">' + elementValue.Venue + '</td>';
+                html += '<td style="color:#3d3939;">' + elementValue.PhoneNumber + '</td>';
+                html += '<td style="color:#3d3939;">' + elementValue.Email + '</td>';
+                html += '<td style="color:#3d3939;">' + DateTime + '</td>';
+                html += '<td style="color:#3d3939;">' + elementValue.Time + '</td>';
+                html += '<td style="color:#3d3939;"><div class="checkbox"><label><input id="activeConditions" class="form-control-sm" type="checkbox" ' + (elementValue.Active==true? "checked='checked'":"") + ' onchange="ActiveDeactive(' + elementValue.Id + ',this)" name="TermsAndCondition" /> </label></div></td>';
+                html += '</tr>';
+                $("#tblClubManagement>tbody").append(html);
+            });
+        }
+    });
+    $("#divLoader").hide();
+};
+
+function ActiveDeactive(id, val) {
+    debugger
+    var type = false;
+    if (val.checked == true) {
+        type = true;
+    }
+    
+    var data = {
+        Id: id,
+        Active: type
+    };
+    $.ajax({
+        url: '/ClubManagement/UpdateClubManagement',
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data:data,
+        success: function (response) {
+            if (response.model == true) {
+                $.alert({
+                    title: '',
+                    content: "Status Updated Successfully.",
+                    type: 'blue'
+                });
+            }
+        }
+    });
+}
---
+++ ShomaRM/Areas/Admin/Controllers/Club/ClubManagementController.cs
@@ -0,0 +1,42 @@
+﻿using ShomaRM.Areas.Tenant.Models.Club;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShomaRM.Areas.Admin.Controllers.Club
+{
+    public class ClubManagementController : Controller
+    {
+        // GET: Admin/ClubManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetClubManagementList(DateTime FromDate, DateTime ToDate)
+        {
+            try
+            {
+                return Json(new { model = new ClubModel().GetClubList() }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception Ex)
+            {
+                return Json(new { model = Ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult UpdateClubManagement(long Id, bool Active)
+        {
+            try
+            {
+                return Json(new { model = new ClubModel().UpdateClubActiveDeactive(Id:Id,Active: Active) }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception Ex)
+            {
+                return Json(new { model = Ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
