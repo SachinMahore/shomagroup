a597527cd6f46ceb1efcceaf5fc10a3cb1e6a1df
Apurva Raut <apurvaraut20@gmail.com>
2020-02-22 05:54:26 -0800

    twlio api work

--- ShomaRM/Controllers/HomeController.cs
+++ ShomaRM/Controllers/HomeController.cs
@@ -1,19 +1,28 @@
 ﻿using ShomaRM.Models;
 using ShomaRM.Models.Bluemoon;
+using ShomaRM.Models.TwilioApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml;
+using Twilio;
+using Twilio.AspNet.Mvc;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML;
 
 namespace ShomaRM.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : TwilioController
     {
         public ActionResult Index()
         {
-            if (Session["DelDatAll"] !=null)
+
+
+
+
+            if (Session["DelDatAll"] != null)
             {
                 if (Session["DelDatAll"].ToString() == "Del")
                 {
@@ -30,17 +39,20 @@ namespace ShomaRM.Controllers
             }
             Session["DelDatAll"] = null;
 
-            
+
             ViewBag.ActiveMenu = "home";
             return View();
         }
 
         public async System.Threading.Tasks.Task<ActionResult> About()
-        
-        
         {
             ViewBag.Message = "Your application description page.";
-           
+
+            TwilioService twilioService = new TwilioService();
+            twilioService.TestSMS("+9547908408");
+            twilioService.TestCall("+9547908408");
+
+
             var data = await testAsync();
             if (data != null)
             {
@@ -52,14 +64,14 @@ namespace ShomaRM.Controllers
 
         public async System.Threading.Tasks.Task<LeaseResponseModel> testAsync()
         {
-            
+
             var test = new BluemoonService();
             LeaseRequestModel leaseRequestModel = new LeaseRequestModel();
             leaseRequestModel.UNIT_NUMBER = "Apurva";
             LeaseResponseModel authenticateData = await test.CreateSession();
-            LeaseResponseModel leaseCreateResponse = await test.CreateLease(leaseRequestModel: leaseRequestModel,PropertyId : "112154",sessionId : authenticateData.SessionId);
+            LeaseResponseModel leaseCreateResponse = await test.CreateLease(leaseRequestModel: leaseRequestModel, PropertyId: "112154", sessionId: authenticateData.SessionId);
             LeaseResponseModel leaseEditResponse = await test.EditLease(leaseRequestModel: leaseRequestModel, leaseId: leaseCreateResponse.LeaseId, sessionId: authenticateData.SessionId);
-            LeaseResponseModel leasePdfResponse = await test.GenerateLeasePdf(sessionId : authenticateData.SessionId,leaseId : leaseCreateResponse.LeaseId);
+            LeaseResponseModel leasePdfResponse = await test.GenerateLeasePdf(sessionId: authenticateData.SessionId, leaseId: leaseCreateResponse.LeaseId);
             await test.CloseSession(sessionId: authenticateData.SessionId);
             return leasePdfResponse;
 
