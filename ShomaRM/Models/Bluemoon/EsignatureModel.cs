1292e5f81e5ac827cf40f329250c5dd379ccf69c
Apurva Raut <apurvaraut20@gmail.com>
2020-02-26 06:07:19 -0800

    esignature Request

---
+++ ShomaRM/Models/Bluemoon/EsignatureModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShomaRM.Models.Bluemoon
+{
+    public class EsignatureParty
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool IsOwner { get; set; }
+    }
+}
