d6f18acbfcc0204b70e4a1e1721edfad8a74f511
vijay-ramteke <55877181+vijay-ramteke@users.noreply.github.com>
2020-02-28 19:30:07 +0530

    changes done

--- ShomaRM/Areas/Admin/Models/Country/CountryModel.cs
+++ ShomaRM/Areas/Admin/Models/Country/CountryModel.cs
@@ -71,6 +71,16 @@ namespace ShomaRM.Areas.Admin.Models
                     paramNOR.Value = model.NumberOfRows;
                     cmd.Parameters.Add(paramNOR);
 
+                    DbParameter paramSortBy = cmd.CreateParameter();
+                    paramSortBy.ParameterName = "SortBy";
+                    paramSortBy.Value = model.SortBy;
+                    cmd.Parameters.Add(paramSortBy);
+
+                    DbParameter paramOrderBy = cmd.CreateParameter();
+                    paramOrderBy.ParameterName = "OrderBy";
+                    paramOrderBy.Value = model.OrderBy;
+                    cmd.Parameters.Add(paramOrderBy);
+
                     DbDataAdapter da = DbProviderFactories.GetFactory("System.Data.SqlClient").CreateDataAdapter();
                     da.SelectCommand = cmd;
                     da.Fill(dtTable);
@@ -117,6 +127,16 @@ namespace ShomaRM.Areas.Admin.Models
                     paramNOR.ParameterName = "NumberOfRows";
                     paramNOR.Value = model.NumberOfRows;
                     cmd.Parameters.Add(paramNOR);
+
+                    DbParameter paramSortBy = cmd.CreateParameter();
+                    paramSortBy.ParameterName = "SortBy";
+                    paramSortBy.Value = model.SortBy;
+                    cmd.Parameters.Add(paramSortBy);
+
+                    DbParameter paramOrderBy = cmd.CreateParameter();
+                    paramOrderBy.ParameterName = "OrderBy";
+                    paramOrderBy.Value = model.OrderBy;
+                    cmd.Parameters.Add(paramOrderBy);
 
                     DbDataAdapter da = DbProviderFactories.GetFactory("System.Data.SqlClient").CreateDataAdapter();
                     da.SelectCommand = cmd;
@@ -185,6 +205,8 @@ namespace ShomaRM.Areas.Admin.Models
         public int PageNumber { get; set; }
         public int NumberOfPages { get; set; }
         public int NumberOfRows { get; set; }
-        public String Criteria { get; set; }
+        public string Criteria { get; set; }
+        public string SortBy { get; set; }
+        public string OrderBy { get; set; }
     }
 }
