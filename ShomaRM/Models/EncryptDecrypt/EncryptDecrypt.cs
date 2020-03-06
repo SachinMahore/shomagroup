a2b18c751df67bb80cb6211665234fd2d9292be8
nirvitech@outlook.com <nirvitech@outlook.com>
2020-03-06 14:19:57 +0530

    EncDec

---
+++ ShomaRM/Models/EncryptDecrypt/EncryptDecrypt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ShomaRM.Models
+{
+    public class EncryptDecrypt
+    {
+        private const string _securityKey = "$h0m@Gr0u9";
+        public string EncryptText(string PlainText)
+        {
+
+            byte[] toEncryptedArray = UTF8Encoding.UTF8.GetBytes(PlainText);
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(_securityKey));
+            objMD5CryptoService.Clear();
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            objTripleDESCryptoService.Key = securityKeyArray;
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+            var objCrytpoTransform = objTripleDESCryptoService.CreateEncryptor();
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptedArray, 0, toEncryptedArray.Length);
+            objTripleDESCryptoService.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        public string DecryptText(string CipherText)
+        {
+            byte[] toEncryptArray = Convert.FromBase64String(CipherText);
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(_securityKey));
+            objMD5CryptoService.Clear();
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+            objTripleDESCryptoService.Key = securityKeyArray;
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+            var objCrytpoTransform = objTripleDESCryptoService.CreateDecryptor();
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            objTripleDESCryptoService.Clear();
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
