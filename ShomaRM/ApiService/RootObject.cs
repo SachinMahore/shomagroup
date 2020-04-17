82fe25ac7912b883e497c87f23250ad6be62408e
lalitbokde <lalitthinker@gmail.com>
2020-04-17 20:35:18 +0530

    Changes Done

---
+++ ShomaRM/ApiService/RootObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShomaRM.ApiService
+{
+    public class RootObject
+    {
+        public string content { get; set; }
+        public List<object> Value { get; set; }
+    }
+}
---
+++ ShomaRM/ApiService/Token.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShomaRM.ApiService
+{
+    public class Token
+    {
+        public string access_token { get; set; }
+    }
+}
